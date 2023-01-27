cd6d11ff20d567c0794de076fdbad35afb4cd68b
mobtgzhang <mobtgzhang@outlook.com>
2023-01-27 21:44:43 +0800

    update some codes

--- uContra/Map.cpp
+++ uContra/Map.cpp
@@ -9,3 +9,38 @@ Map::Map(SDL_Renderer* rR){
 Map::~Map(){
     
 }
+
+// 第一关
+void Map::load_Jungle(){
+
+}
+
+// 第二关
+void Map::load_MazeBase1(){
+    
+}
+// 第三关
+void Map::load_WaterFall(){
+
+}
+// 第四关
+void Map::load_MazeBase2(){
+
+}
+// 第五关
+void Map::load_SnowField(){
+
+}
+// 第六关
+void Map::load_EnergyZone(){
+
+}
+// 第七关
+void Map::load_Hangar(){
+
+}
+//第八关
+void Map::load_AlienLair(){
+
+}
+
--- uContra/Map.h
+++ uContra/Map.h
@@ -7,6 +7,17 @@
 class Map{
 private:
     float fXpos,fYPos;
+
+    void load_Jungle(); // 第一关
+    void load_MazeBase1(); // 第二关
+    void load_WaterFall(); // 第三关
+    void load_MazeBase2(); // 第四关
+    void load_SnowField(); // 第五关
+    void load_EnergyZone(); // 第六关
+    void load_Hangar(); // 第七关
+    void load_AlienLair(); //第八关
+
+
 public:
     Map();
     Map(SDL_Renderer* rR);
